bb53bcb5c3748f0a67168940ceb495e32e175553
bosmanpa <bsmn.ptrck@gmail.com>
2019-11-05 11:30:26 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,4 @@ clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
 clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
  
 movies = Category.create!(name: "Movies")
-movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
+movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")l
